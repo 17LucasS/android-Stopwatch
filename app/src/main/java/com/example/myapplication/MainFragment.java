c7c490b1dba03ef81c47e1d871add1cdc01fafde
Lukas S <lucascode@interia.pl>
2023-08-03 10:24:21 +0200

    Add fragment_main.xml/add style Textview

---
+++ app/src/main/java/com/example/myapplication/MainFragment.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    TextView textViewStopWatcher;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        textViewStopWatcher = view.findViewById(R.id.textView_display_stop_watcher);
+
+        return view;
+
+    }
+}
